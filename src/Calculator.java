ea39ad466e1f18bb5d21fbb6386aaff3e7c82a02
KumarSChandra <58375738+KumarSChandra@users.noreply.github.com>
2020-01-29 00:08:24 -0800

    ayay it wrokds

---
+++ src/Calculator.java
@@ -0,0 +1,46 @@
+public class Calculator {
+    String typings[] = {"Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost",
+            "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy"};
+    double normal[] = {1, 1, 1, 1, 1, .5, 1, 0, .5, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    double fighting[] = {2, 1, .5, .5, 1, 2, .5, 0, 2, 1, 1, 1, 1, .5, 2, 1, 2, .5};
+    double flying[] = {1, 2, 1, 1, 1, .5, 2, 1, .5, 1, 1, 2, .5, 1, 1, 1, 1, 1};
+    double poison[] = {1, 1, 1, .5, .5, .5, 1, .5, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2};
+    double ground[] = {1, 1, 0, 2, 1, 2, .5, 1, 2, 2, 1, .5, 2, 1, 1, 1, 1, 1};
+    double rock[] = {1, .5, 2, 1, .5, 1, 2, 1, .5, 2, 1, 1, 1, 1, 2, 1, 1, 2, .5};
+    double bug[] = {1, .5, .5, .5, 1, 1, 1, .5, .5, .5, 1, 2, 1, 2, 1, 1, 2, .5};
+    double ghost[] = {0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, .5, 1};
+    double steel[] = {1, 1, 1, 1, 1, 2, 1, 1, .5, .5, .5, 1, .5, 1, 2, 1, 1, 2};
+    double fire[] = {1, 1, 1, 1, 1, .5, 2, 1, 2, .5, .5, 2, 1, 1, 2, .5, 1, 1};
+    double water[] = {1, 1, 1, 1, 2, 2, 1, 1, 1, 2, .5, .5, 1, 1, 1, .5, 1, 1};
+    double grass[] = {1, 1, .5, .5, 2, 2, .5, 1, .5, .5, 2, .5, 1, 1, 1, .5, 1, 1};
+    double electric[] = {1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 2, .5, .5, 1, 1, .5, 1, 1};
+    double psychic[] = {1, 2, 1, 2, 1, 1, 1, 1, .5, 1, 1, 1, 1, .5, 1, 1, 0, 1};
+    double ice[] = {1, 1, 2, 1, 2, 1, 1, 1, .5, .5, .5, 2, 1, 1, .5, 2, 1, 1};
+    double dragon[] = {1, 1, 1, 1, 1, 1, 1, 1, .5, 1, 1, 1, 1, 1, 1, 2, 1, 0};
+    double dark[] = {1, .5, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, .5, .5};
+    double fairy[] = {1, 2, 1, .5, 1, 1, 1, 1, .5, .5, 1, 1, 1, 1, 1, 2, 2, 1};
+    double typingArray[][] = {normal, fighting, flying, poison, ground, rock, bug, ghost, steel, fire,
+            water, grass, electric, psychic, ice, dragon, dark, fairy};
+
+    public int getIntFromType(String type) {
+        int answer = 0;
+        for (int i = 0; i < typings.length; i++) {
+            if (type.equalsIgnoreCase(typings[i])) {
+                answer = i;
+                break;
+            }
+
+        }
+        return answer;
+    }
+
+
+
+        public double modifier(int attackType, int defendType1, int defendType2) {
+            double answer=0;
+            answer = typingArray[attackType][defendType1]*typingArray[attackType][defendType2];
+            return answer;
+        }
+
+
+    }
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String [] args) {
+        EaseOfUse ez = new EaseOfUse();
+        Calculator calc = new Calculator();
+Scanner kboard = new Scanner(System.in);
+        ez.print("Enter the type of the attack");
+        String attackType= kboard.next();
+ez.print("Enter your first type of the defending pokemon");
+        String type1= kboard.next();
+        ez.print("Enter your second type of the defending pokemon");
+        String type2= kboard.next();
+        int num1 =calc.getIntFromType(type1), num2 =calc.getIntFromType(type2), num0 =calc.getIntFromType(attackType);
+        double mod =calc.modifier(num0,num1, num2);
+        ez.print("Your attack damage will be multiplied by this much\n"+mod);
+    }
+}
