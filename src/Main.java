885d4cb2bd1f957ea43d4d212cf859e1ac1b2452
kchandra423 <kchandra423@student.fuhsd.org>
2020-01-29 15:21:17 -0800

    Bug Fixes
    dual type now works properly

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main (String [] args) {
         EaseOfUse ez = new EaseOfUse();
         CheckInput myobj = new CheckInput();
         Calculator calc = new Calculator();
-      
+        Scanner kboard = new Scanner(System.in);
+        boolean loopchecker=true;
+      while(loopchecker) {
         String x= myobj.getMessage("Is the defender monotype or dualtype?", "Mono Monotype Dualtype dual");
         
         String attackType = myobj.getMessage("Enter the type of the attack", "Normal Fighting Flying Poison Ground Rock Bug Ghost Steel Fire Water Grass Electric Psychic Ice Dragon Dark Fairy");
@@ -21,10 +23,16 @@ public class Main {
 else {
 	String defendType = myobj.getMessage("What is the type of the defender", "Normal Fighting Flying Poison Ground Rock Bug Ghost Steel Fire Water Grass Electric Psychic Ice Dragon Dark Fairy");
  num1=calc.getIntFromType(defendType);
-		  num2=0;
+		  num2=-1;
 	
 }
 double mod =calc.modifier(num0,num1, num2);
         ez.print("Your attack damage will be multiplied by:\n"+mod);
-    }
+         ez.print("Do you want to quit the program");
+        String z= kboard.next();
+        if(z.equalsIgnoreCase("yes")) {
+        	loopchecker=false;
+        }
+      }}
+    
 }
